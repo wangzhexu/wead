c60ba093c1f845b8c43b64914af84695fd2719be
wangzhexu <test@wangzhexu.com>
2017-12-07 11:25:46 +0800

    第三次提交

---
+++ src/test/aaa.java
@@ -0,0 +1,8 @@
+package test;
+
+public class aaa {
+
+	public static void main(String[] args) {
+		String aa = "sssssssssssssssssssss";
+	}
+	}
--- src/test/dome.java
+++ src/test/dome.java
@@ -3,5 +3,6 @@ package test;
 public class dome {
 public static void main(String[] args) {
 	String aa = "sssssssssssssssssssss";
+	System.out.println("sssssssssssssssssssss");
 }
 }
