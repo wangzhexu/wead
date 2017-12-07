6028f067a13237ac454acdb6b51fbb585c26f417
wangzhexu <test@wangzhexu.com>
2017-12-07 11:34:22 +0800

    第四次添加

--- src/test/dome.java
+++ src/test/dome.java
@@ -4,5 +4,6 @@ public class dome {
 public static void main(String[] args) {
 	String aa = "sssssssssssssssssssss";
 	System.out.println("sssssssssssssssssssss");
+	System.out.println("ssssssssssssssssssssssss");
 }
 }
